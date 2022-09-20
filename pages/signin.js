31ad8118ff71eed7554e6a318137f84d0c62066c
Valentin Micu <valvic6@gmail.com>
2022-09-20 16:53:20 +0300

    removed on click event handler from signin page

--- pages/signin.js
+++ pages/signin.js
@@ -1,10 +1,5 @@
-import axios from 'axios';
 import React from 'react';
 
-export default function signin() {
-  return (
-    <button className="pt-20" onClick={signUp}>
-      signup
-    </button>
-  );
+export default function Signin() {
+  return <button className="pt-20">signup</button>;
 }
