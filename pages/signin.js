a7a4c2f18873f9bb7c8763ca3bade2531c77231d
Valentin Micu <valvic6@gmail.com>
2022-09-29 12:50:26 +0300

    installed material ui; replaced input on sign up and sign in page

--- pages/signin.js
+++ pages/signin.js
@@ -4,6 +4,8 @@ import { useRouter } from 'next/router';
 import { API_ROUTES, APP_ROUTES } from '../constants/routes';
 import { storeToLocalStorage } from '../utils';
 import Link from 'next/link';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Signin() {
   const [userData, setUserData] = useState({ email: '', password: '' });
@@ -55,57 +57,49 @@ export default function Signin() {
 
   return (
     <div className="pt-20 w-full max-w-xs mx-auto">
-      <div className="bg-white shadow-md rounded px-8 pt-6">
+      <div className="bg-white shadow-md rounded px-4 pt-6">
+        <p className="text-lg text-blue-700 pb-2 pl-2">Please sign in</p>
         <form autoComplete="off">
           <input type="hidden" value="something”" />
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <label htmlFor="email" />
+            <TextField
+              className="w-full"
+              required
               id="email"
               name="email"
-              placeholder="email"
               autoFocus
-              required
+              label="Email"
               onChange={handleChange}
               value={userData.email}
               autoComplete="off"
             />
           </div>
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <label htmlFor="password" />
+            <TextField
+              className="w-full"
+              required
               id="password"
               name="password"
-              placeholder="password"
-              required
+              label="Password"
               onChange={handleChange}
               value={userData.password}
               autoComplete="off"
             />
           </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex"
+          <Button
+            className=""
             type="submit"
             disabled={!userData.email || !userData.password}
             onClick={signIn}
+            variant="outlined"
           >
             {isLoading ? (
               <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" />
             ) : null}
             Sign in
-          </button>
+          </Button>
         </form>
         {error && <p className="text-red-600 text-xs pt-2">{error}</p>}
         <div className="flex items-center justify-evenly">
--- pages/signup.js
+++ pages/signup.js
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { API_ROUTES, APP_ROUTES } from '../constants/routes';
 import { storeToLocalStorage } from '../utils';
 import { useRouter } from 'next/router';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 // implement Formik later
 export default function Signup() {
@@ -57,57 +59,49 @@ export default function Signup() {
 
   return (
     <div className="pt-20 w-full max-w-xs mx-auto">
-      <div className="bg-white shadow-md rounded px-8 pt-6">
+      <div className="bg-white shadow-md rounded px-4 pt-6">
+        <p className='text-lg text-blue-700 pb-2 pl-2'>Please sign up</p>
         <form className="" autoComplete="off">
           <input type="hidden" value="something”" />
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <label htmlFor="email" />
+            <TextField
+              className='w-full'
+              required
               id="email"
               name="email"
-              placeholder="email"
               autoFocus
-              required
+              label="Email"
               onChange={handleChange}
               value={userData.email}
               autoComplete="off"
             />
           </div>
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <label htmlFor="password" />
+            <TextField
+              className='w-full'
+              required
               id="password"
               name="password"
-              placeholder="password"
-              required
+              label="Password"
               onChange={handleChange}
               value={userData.password}
               autoComplete="off"
             />
           </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex"
+          <Button
+            className=""
             type="submit"
             disabled={!userData.email || !userData.password}
             onClick={signUp}
+            variant="outlined"
           >
             {isLoading ? (
               <div className="mr-2 w-5 h-5 border-l-2 rounded-full animate-spin" />
             ) : null}
             Sign up
-          </button>
+          </Button>
         </form>
         {error && <p className="text-red-600 text-xs pt-2">{error}</p>}
         <div className="flex items-center justify-evenly">
