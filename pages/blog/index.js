b775cba8146592b4bc8555c46701480ff8977a83
Valentin Micu <valvic6@gmail.com>
2022-12-07 11:15:46 +0200

    pagination setup and initial work

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BlogPost } from '@/components/BlogPost';
-// import { API_ROUTES } from '@/constants/routes';
-// import { client } from '../../axios';
-import { getAllBlogs } from '@/lib/data';
+import { getPaginatedBlogs } from '@/lib/data';
+import useSWR from 'swr';
+import request from 'graphql-request';
+import Button from '@mui/material/Button';
+
+const fetchData = (endpoint, query, params) => request(endpoint, query, params);
+const API_URL =
+  'https://api-eu-central-1.hygraph.com/v2/cl7qpydxj5mv601t78887184a/master';
 
 function AllPosts({ blogs }) {
+  const [first, setFirst] = useState(2);
+  const [skip, setSkip] = useState(0);
+
+  const { data, err } = useSWR(
+    [
+      API_URL,
+      `
+    query paginatedBlogs($first: Int, $skip: Int) {
+      blogModels(first: $first, skip: $skip) {
+        id
+        slug
+        title
+        subheading
+        tags
+        content {
+          html
+          markdown
+        }
+        bannerImage {
+          height
+          size
+          width
+          url
+          fileName
+        }
+        date
+      }
+    }
+    `,
+      first,
+      skip,
+    ],
+    (endpoint, query) => fetchData(endpoint, query, { first, skip }),
+    { initialData: blogs, revalidateOnFocus: false }
+  );
+  console.table(data?.blogModels);
+
+  const handlePrevClick = () => {
+    setSkip(skip - 2);
+  };
+
+  const handleNextClick = () => {
+    setSkip(skip + 2);
+  };
+
   return (
     <>
       {/* {IS_POST_SEARCH_ENABLED && <PostSearch onSearch={handleSearch} />} */}
       <div className="grid grid-cols-2 md:grid-cols-3 gap-2 p-2">
-        {blogs?.map((post, key) => {
+        {data?.blogModels.map((post, key) => {
           return (
             <div key={key}>
               <BlogPost post={post} />
@@ -17,6 +67,8 @@ function AllPosts({ blogs }) {
           );
         })}
       </div>
+      <Button onClick={handlePrevClick}>Previous</Button>
+      <Button onClick={handleNextClick}>Next</Button>
     </>
   );
   // const router = useRouter();
@@ -29,11 +81,12 @@ function AllPosts({ blogs }) {
 }
 
 export async function getStaticProps() {
-  const data = await getAllBlogs();
+  // const data = await getAllBlogs();
+  const initialData = await getPaginatedBlogs(2, 0);
 
   return {
     props: {
-      blogs: data.blogModels,
+      blogs: initialData.blogModels,
     },
   };
 }
