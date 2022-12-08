0605a232d5f0db5ec80f505292405d7d2b38c442
Valentin Micu <valvic6@gmail.com>
2022-12-08 12:33:31 +0200

    fixed failing build; added key to iteration

--- components/PostTag.jsx
+++ components/PostTag.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const PostTag = ({ tags }) => {
   return (
     <section className="">
-      {tags?.map((tag) => (
-        <span className="text-sm sm:text-base mr-3">#{tag}</span>
+      {tags?.map((tag, key) => (
+        <span key={key} className="text-sm sm:text-base mr-3">#{tag}</span>
       ))}
     </section>
   );
