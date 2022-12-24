35d27381dd782884aaa0d43699272c6dff2a09f2
Valentin Micu <valvic6@gmail.com>
2022-12-24 12:10:43 +0200

    implemented search - initial variant

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,47 @@
 import { BlogPostCard } from '@/components/BlogPostCard';
 import { getPosts } from '@/lib/posts';
+import TextField from '@mui/material/TextField';
+import { useEffect, useState } from 'react';
 
 export default function Blog({ allPosts }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [postNames, setPostNames] = useState(
+    allPosts.map((post) => post.title)
+  );
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    const posts = [...allPosts];
+
+    const filteredPosts = posts.filter((post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    );
+
+    setFilteredPosts(filteredPosts);
+  }, [searchTerm]);
+
+  const activePosts = searchTerm.length < 0 ? allPosts : filteredPosts;
+
   return (
     <section className="max-w-[860px] mx-auto">
       {/* {IS_POST_SEARCH_ENABLED && <PostSearch onSearch={handleSearch} />} */}
+      <div className="mb-4">
+        <label htmlFor="search" />
+        <TextField
+          fullWidth
+          id="search"
+          name="search"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="search"
+        />
+      </div>
       <div className="grid gap-1">
-        {allPosts?.map((post, key) => (
+        {activePosts?.map((post, key) => (
           <BlogPostCard post={post} key={key} />
         ))}
       </div>
