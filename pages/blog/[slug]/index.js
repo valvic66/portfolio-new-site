915239132fbc31d2001f7d1bd8e02078b1a21677
Valentin Micu <valvic6@gmail.com>
2023-03-24 14:58:43 +0200

    added progress bar on scroll blog pages

--- pages/blog/[slug]/index.js
+++ pages/blog/[slug]/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SvgSpinner } from '@/components/SvgSpinner';
 import { BlogPost } from '@/components/BlogPost';
 import { getPosts } from '@/lib/posts';
 import md from 'markdown-it';
 
 function PostDetailPage({ postBySlug }) {
-  const content = postBySlug?.content ?? '';
+  const [scroll, setScroll] = useState(0);
 
+  useEffect(() => {
+    const progressBarScroll = () => {
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scroll = `${totalScroll / windowHeight}`;
+
+      setScroll(scroll);
+    };
+
+    window.addEventListener('scroll', progressBarScroll);
+
+    return () => window.removeEventListener('scroll', progressBarScroll);
+  }, []);
+
+  const content = postBySlug?.content ?? '';
+  console.log({ scroll });
   return (
     <main className="prose max-w-[860px] mx-auto">
+      <div id="progressBarContainer">
+        <div
+          id="progressBar"
+          style={{
+            transform: `scale(${scroll}, 1)`,
+            // opacity: `${scroll}`,
+          }}
+        />
+      </div>
       <BlogPost post={postBySlug} />
       <div
         className="p-5"
